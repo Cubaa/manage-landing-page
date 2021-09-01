12ee732e3f830c19dec2b5a43cc261d81b328c9b
Cubaa <jakub29-11@wp.pl>
2021-09-01 14:05:46 +0200

    adding incomplete slider
    adding incomplete slider

--- src/components/Slider/Slide/Slide.tsx
+++ src/components/Slider/Slide/Slide.tsx
@@ -1,10 +1,69 @@
 import React from "react";
+import styled from "styled-components";
+
+const SingleSlide = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0 15px;
+  background-color: #ebecf0;
+  padding: 0.7rem 0.8rem;
+  min-height: 250px;
+  border-radius: 8px;
+  z-index: 995;
+`;
+const SlideImage = styled.div`
+  position: absolute;
+  top: -40px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  z-index: 999;
+  border: 1px solid black;
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+`;
+const SlideInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span {
+    display: inline-block;
+    margin: 20px 0;
+    font-weight: 700;
+    font-size: 15px;
+  }
+  p {
+    font-size: 14px;
+    text-align: center;
+    color: hsl(227, 12%, 61%);
+  }
+`;
 
 interface ISlide {
   name: string;
   content: string;
+  image: string;
 }
 
-export const Slide: React.FC<ISlide> = (props) => {
-  return <></>;
+export const Slide: React.FC<ISlide> = ({ name, content, image }) => {
+  return (
+    <>
+      <SingleSlide>
+        <SlideImage>
+          <img src={image} alt="image" />
+        </SlideImage>
+        <SlideInfo>
+          <span>{name}</span>
+          <p>{content}</p>
+        </SlideInfo>
+      </SingleSlide>
+    </>
+  );
 };
--- src/components/Slider/SliderContainer.tsx
+++ src/components/Slider/SliderContainer.tsx
@@ -5,27 +5,60 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { sliderData } from "../../data/sliderData";
 import { Slide } from "./Slide/Slide";
+import styled from "styled-components";
+import { Button } from "../Button/Button";
 
+const SliderContainerStyles = styled.section`
+  width: 100%;
+  padding: 1rem;
+  margin-top: 100px;
+  overflow-x: hidden;
+  z-index: 990;
+  h2 {
+    text-align: center;
+    margin-bottom: 50px;
+    color: hsl(233, 12%, 13%);
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0;
+`;
 export const SliderContainer: React.FC = () => {
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    initialSlide: 0,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    autoplay: true,
   };
   return (
     <>
-      <div>
-        <h2> Single Item</h2>
+      <SliderContainerStyles>
+        <h2> What they've said</h2>
         <Slider {...settings}>
           {sliderData.map((item, index) => {
             return (
-              <Slide key={index} name={item.name} content={item.content} />
+              <Slide
+                key={index}
+                image={item.image}
+                name={item.name}
+                content={item.content}
+              />
             );
           })}
         </Slider>
-      </div>
+        <ButtonWrapper>
+          <Button size="big">Get started </Button>
+        </ButtonWrapper>
+      </SliderContainerStyles>
     </>
   );
 };
