965d24a86ce6aab0868cece66cc0df7bfb12a515
Cubaa <jakub29-11@wp.pl>
2021-09-13 21:31:35 +0200

    Test2 - commit
    Test2 - commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { FooterLinks } from "./FooterLinks/FooterLinks";
-import { InputAndCopyright } from "./InputAndCopyright/InputAndCopyright";
+import { InputAndCopyright } from "./InputAndCopyright/InputAndcopyright";
 import { SocialMediaLogo } from "./SocialMediaLogo/SocialMediaLogo";
 
 const FooterWrapper = styled.footer`
--- src/data/sliderData.ts
+++ src/data/sliderData.ts
@@ -1,24 +1,24 @@
 export const sliderData = [
   {
-    image: "../images/avatar-anisha.PNG",
+    image: "../images/avatar-anisha.png",
     name: "Anisha Li",
     content:
       "“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
   },
   {
-    image: "../images/avatar-ali.PNG",
+    image: "../images/avatar-ali.png",
     name: "Ali Bravo",
     content:
       "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.” ",
   },
   {
-    image: "../images/avatar-richard.PNG",
+    image: "../images/avatar-richard.png",
     name: "Richard Watts",
     content:
       "“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”",
   },
   {
-    image: "../images/avatar-shanai.PNG",
+    image: "../images/avatar-shanai.png",
     name: "Shanai Gough",
     content:
       "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
